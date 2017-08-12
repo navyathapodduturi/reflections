cab31038f206a6b47d74ababcfed92936308dd25
navya <navyathareddy23@gmail.com>
2017-08-12 13:05:26 -0400

    where loop

---
+++ wherpy.py
@@ -0,0 +1,29 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 10
+>>> y = 0
+>>> while y<10:
+	print 'hello here-',y
+	y += 1
+
+	 
+hello here- 0
+hello here- 1
+hello here- 2
+hello here- 3
+hello here- 4
+hello here- 5
+hello here- 6
+hello here- 7
+hello here- 8
+hello here- 9
+>>> while y<10:
+	print 'hello here-',y
+	y += 1
+	 else:
+		 
+  File "<pyshell#7>", line 5
+    else:
+    ^
+IndentationError: unexpected indent
+>>> 
